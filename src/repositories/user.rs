1b5e449097732e4c61d9794e78872239609376de
Jakub Baran <baran.jaqb@gmail.com>
2023-07-25 00:34:50 +0200

    #3 implementing user repository - crud operations

--- src/repositories/user.rs
+++ src/repositories/user.rs
@@ -1,5 +1,7 @@
 use async_trait::async_trait;
-use crate::database::{CONNECTION, Database};
+use sqlx::{query, query_as};
+use sqlx::mysql::MySqlQueryResult;
+use crate::database::{Database};
 use crate::entities::User;
 use crate::repositories::Repository;
 
@@ -7,26 +9,68 @@ pub struct UserRepository {}
 
 #[async_trait]
 impl Repository<User> for UserRepository {
-    async fn get_all() -> Vec<User> {
-    sqlx::query_as::<_, User>("SELECT id, username, email, password, wallet FROM user")
-        .fetch_all(Database::get_connection())
-        .await
-        .unwrap().to_owned()
+    const SELECT_ALL_QUERY: &'static str = "\
+        SELECT id, username, email, password, wallet \
+        FROM user";
+    async fn get_all() -> Option<Vec<User>> {
+        query_as::<_, User>(Self::SELECT_ALL_QUERY)
+            .fetch_all(Database::get_connection())
+            .await
+            .ok()
     }
 
-    // fn get_by_id(id: u64) -> User {
-    //     todo!()
-    // }
-    //
-    // fn create(entity: User) {
-    //     todo!()
-    // }
-    //
-    // fn update(entity: User) {
-    //     todo!()
-    // }
-    //
-    // fn delete(entity: User) {
-    //     todo!()
-    // }
+    const SELECT_BY_ID_QUERY: &'static str = "\
+        SELECT id, username, email, password, wallet \
+        FROM user \
+        WHERE id = (?)";
+    async fn get_by_id(id: u64) -> Option<User> {
+        query_as::<_, User>(Self::SELECT_BY_ID_QUERY)
+            .bind(id)
+            .fetch_one(Database::get_connection())
+            .await
+            .ok()
+    }
+
+    const INSERT_QUERY: &'static str = "\
+        INSERT INTO user (username, email, password, wallet) \
+        VALUES (?, ?, ?, ?)";
+    async fn create(entity: &User) -> Option<User> {
+        let insert_result: MySqlQueryResult = query(Self::INSERT_QUERY)
+            .bind(entity.username.as_str())
+            .bind(entity.email.as_str())
+            .bind(entity.password.as_str())
+            .bind(entity.wallet)
+            .execute(Database::get_connection()).await
+            .expect(format!("Unable to create new User: {:?}", entity).as_str());
+
+        Self::get_by_id(insert_result.last_insert_id()).await
+    }
+
+    const UPDATE_QUERY: &'static str = "\
+        UPDATE user \
+        SET username = ?, email = ?, password = ?, wallet = ? \
+        WHERE id = ?";
+    async fn update(id: u64, entity: &User) -> Option<User> {
+        query(Self::UPDATE_QUERY)
+            .bind(entity.username.as_str())
+            .bind(entity.email.as_str())
+            .bind(entity.password.as_str())
+            .bind(entity.wallet)
+            .bind(id)
+            .execute(Database::get_connection()).await
+            .expect(format!("Unable to create new User: {:?}", entity).as_str());
+
+        Self::get_by_id(id).await
+    }
+
+    const DELETE_QUERY: &'static str = "\
+        DELETE FROM user \
+        WHERE id = ?";
+    async fn delete(id: u64) {
+        sqlx::query(Self::DELETE_QUERY)
+            .bind(id)
+            .execute(Database::get_connection())
+            .await
+            .expect(format!("Unable to delete user with id: {}", id).as_str());
+    }
 }
--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,6 +1,6 @@
 use axum::{Json, Router};
+use axum::extract::Path;
 use axum::routing::{delete, get, post, put};
-use sqlx::MySqlPool;
 use crate::entities::User;
 use crate::repositories::Repository;
 use crate::repositories::user::UserRepository;
@@ -8,15 +8,44 @@ use crate::repositories::user::UserRepository;
 pub fn routes() -> Router {
     Router::new()
         .route("/users", get(get_users))
-    // .route("/users", post(create_user))
-    // .route("/users/:id", get(get_user))
-    // .route("/users/:id", put(update_user))
-    // .route("/users/:id", delete(delete_user))
+    .route("/users", post(create_user))
+    .route("/users/:id", get(get_user))
+    .route("/users/:id", put(update_user))
+    .route("/users/:id", delete(delete_user))
 }
 
 async fn get_users() -> Json<Vec<User>> {
     Json(
         UserRepository::get_all().await
+            .expect("No users")
     )
 }
+
+async fn get_user(Path(id): Path<u64>) -> Json<User> {
+    Json(
+        UserRepository::get_by_id(id).await
+            .expect(format!("Not found user with id: {}", id).as_str())
+    )
+}
+
+async fn create_user(Json(body): Json<User>) -> Json<User> {
+    Json(
+        UserRepository::create(&body).await
+            .expect(format!("Not able to create user: {:?}", &body).as_str()
+        )
+    )
+}
+
+async fn update_user(Path(id): Path<u64>, Json(body): Json<User>) -> Json<User> {
+    Json(
+        UserRepository::update(id, &body).await
+            .expect(format!("Not able to update user (id:{}) with payload: {:?}", id, &body).as_str()
+            )
+    )
+}
+
+async fn delete_user(Path(id): Path<u64>) {
+    UserRepository::delete(id).await;
+}
+
 
