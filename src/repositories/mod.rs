9af9afc2e82fc444c1f3745fccb2e8bbeee1a88c
Jakub Baran <kubabaran2000@wp.pl>
2023-07-25 00:34:50 +0200

    #3 implementing user repository - crud operations

--- src/repositories/mod.rs
+++ src/repositories/mod.rs
@@ -1,12 +1,22 @@
 use async_trait::async_trait;
+use sqlx::{FromRow, Row};
 
 pub mod user;
 
 #[async_trait]
 pub trait Repository<T> {
-    async fn get_all() -> Vec<T>;
-    // async fn get_by_id(id: u64) -> T;
-    // async fn create(entity: T);
-    // async fn update(entity: T);
-    // async fn delete(entity: T);
+    const SELECT_ALL_QUERY: &'static str;
+    async fn get_all() -> Option<Vec<T>>;
+
+    const SELECT_BY_ID_QUERY: &'static str;
+    async fn get_by_id(id: u64) -> Option<T>;
+
+    const INSERT_QUERY: &'static str;
+    async fn create(entity: &T) -> Option<T>;
+
+    const UPDATE_QUERY: &'static str;
+    async fn update(id: u64, entity: &T) -> Option<T>;
+
+    const DELETE_QUERY: &'static str;
+    async fn delete(id: u64);
 }
