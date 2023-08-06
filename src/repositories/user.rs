0d54e15d0d40c64a5a3a5958e552e2d6cf3167b0
Jakub Baran <kubabaran2000@wp.pl>
2023-08-07 00:55:37 +0200

    #5 responses with correct status codes plus basic error handling

--- src/repositories/user.rs
+++ src/repositories/user.rs
@@ -1,8 +1,9 @@
 use async_trait::async_trait;
+use axum::http::StatusCode;
 use sqlx::{query, query_as};
-use sqlx::mysql::MySqlQueryResult;
 use crate::database::{Database};
 use crate::entities::User;
+use crate::error::CustomError;
 use crate::repositories::Repository;
 
 pub struct UserRepository {}
@@ -12,65 +13,93 @@ impl Repository<User> for UserRepository {
     const SELECT_ALL_QUERY: &'static str = "\
         SELECT id, username, email, password, wallet \
         FROM user";
-    async fn get_all() -> Option<Vec<User>> {
-        query_as::<_, User>(Self::SELECT_ALL_QUERY)
+    async fn get_all() -> Result<Vec<User>, CustomError> {
+        match query_as::<_, User>(Self::SELECT_ALL_QUERY)
             .fetch_all(Database::get_connection())
-            .await
-            .ok()
+            .await {
+            Ok(users) => Ok(users),
+            Err(err) => Err(CustomError::new(
+                StatusCode::NOT_FOUND,
+                "Cannot fetch users".to_string(),
+                err.to_string())
+            )
+        }
     }
 
     const SELECT_BY_ID_QUERY: &'static str = "\
         SELECT id, username, email, password, wallet \
         FROM user \
         WHERE id = (?)";
-    async fn get_by_id(id: u64) -> Option<User> {
-        query_as::<_, User>(Self::SELECT_BY_ID_QUERY)
+    async fn get_by_id(id: u64) -> Result<User, CustomError> {
+        match query_as::<_, User>(Self::SELECT_BY_ID_QUERY)
             .bind(id)
             .fetch_one(Database::get_connection())
-            .await
-            .ok()
+            .await {
+            Ok(user) => Ok(user),
+            Err(err) => Err(CustomError::new(
+                StatusCode::NOT_FOUND,
+                "User with given ID does not exists".to_string(),
+                err.to_string()))
+        }
     }
 
     const INSERT_QUERY: &'static str = "\
         INSERT INTO user (username, email, password, wallet) \
         VALUES (?, ?, ?, ?)";
-    async fn create(entity: &User) -> Option<User> {
-        let insert_result: MySqlQueryResult = query(Self::INSERT_QUERY)
+    async fn create(entity: &User) -> Result<User, CustomError> {
+        let insert_result = query(Self::INSERT_QUERY)
             .bind(entity.username.as_str())
             .bind(entity.email.as_str())
             .bind(entity.password.as_str())
             .bind(entity.wallet)
-            .execute(Database::get_connection()).await
-            .expect(format!("Unable to create new User: {:?}", entity).as_str());
+            .execute(Database::get_connection())
+            .await;
 
-        Self::get_by_id(insert_result.last_insert_id()).await
+        match insert_result {
+            Ok(result) => Self::get_by_id(result.last_insert_id()).await,
+            Err(err) => Err(CustomError::new(
+                StatusCode::UNPROCESSABLE_ENTITY,
+                "Cannot create user entity".to_string(),
+                err.to_string()
+            ))
+        }
     }
 
     const UPDATE_QUERY: &'static str = "\
         UPDATE user \
         SET username = ?, email = ?, password = ?, wallet = ? \
         WHERE id = ?";
-    async fn update(id: u64, entity: &User) -> Option<User> {
-        query(Self::UPDATE_QUERY)
+    async fn update(id: u64, entity: &User) -> Result<User, CustomError> {
+        match query(Self::UPDATE_QUERY)
             .bind(entity.username.as_str())
             .bind(entity.email.as_str())
             .bind(entity.password.as_str())
             .bind(entity.wallet)
             .bind(id)
-            .execute(Database::get_connection()).await
-            .expect(format!("Unable to create new User: {:?}", entity).as_str());
-
-        Self::get_by_id(id).await
+            .execute(Database::get_connection()).await {
+            Ok(_) => Self::get_by_id(id).await,
+            Err(err) => Err(CustomError::new(
+                StatusCode::UNPROCESSABLE_ENTITY,
+                "Cannot update desired user entity".to_string(),
+                err.to_string()
+            ))
+        }
     }
 
     const DELETE_QUERY: &'static str = "\
         DELETE FROM user \
         WHERE id = ?";
-    async fn delete(id: u64) {
-        sqlx::query(Self::DELETE_QUERY)
+    async fn delete(id: u64) -> Result<(), CustomError> {
+        match query(Self::DELETE_QUERY)
             .bind(id)
             .execute(Database::get_connection())
-            .await
-            .expect(format!("Unable to delete user with id: {}", id).as_str());
+            .await {
+            Ok(_) => Ok(()),
+            Err(err) => Err(CustomError::new(
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Cannot delete user entity with given ID".to_string(),
+                err.to_string()
+            ))
+        }
     }
 }
--- src/routes/user.rs
+++ src/routes/user.rs
@@ -1,5 +1,7 @@
 use axum::{Json, Router};
-use axum::extract::Path;
+use axum::extract::{Path};
+use axum::http::{StatusCode};
+use axum::response::{IntoResponse, Response};
 use axum::routing::{delete, get, post, put};
 use crate::entities::User;
 use crate::repositories::Repository;
@@ -8,44 +10,37 @@ use crate::repositories::user::UserRepository;
 pub fn routes() -> Router {
     Router::new()
         .route("/users", get(get_users))
-    .route("/users", post(create_user))
-    .route("/users/:id", get(get_user))
-    .route("/users/:id", put(update_user))
-    .route("/users/:id", delete(delete_user))
+        .route("/users", post(create_user))
+        .route("/users/:id", get(get_user))
+        .route("/users/:id", put(update_user))
+        .route("/users/:id", delete(delete_user))
 }
 
-async fn get_users() -> Json<Vec<User>> {
-    Json(
-        UserRepository::get_all().await
-            .expect("No users")
-    )
+async fn get_users() -> Response {
+     match UserRepository::get_all().await {
+        Ok(users) => Json(users).into_response(),
+        Err(err) => err.into_response()
+    }
 }
 
-async fn get_user(Path(id): Path<u64>) -> Json<User> {
-    Json(
-        UserRepository::get_by_id(id).await
-            .expect(format!("Not found user with id: {}", id).as_str())
-    )
+async fn get_user(Path(id): Path<u64>) -> Response {
+        UserRepository::get_by_id(id).await.into_response()
 }
 
-async fn create_user(Json(body): Json<User>) -> Json<User> {
-    Json(
-        UserRepository::create(&body).await
-            .expect(format!("Not able to create user: {:?}", &body).as_str()
-        )
-    )
+async fn create_user(Json(body): Json<User>) -> Response {
+   match UserRepository::create(&body).await {
+       Ok(user) => user.into_response(StatusCode::CREATED),
+       Err(err) => err.into_response()
+   }
 }
 
-async fn update_user(Path(id): Path<u64>, Json(body): Json<User>) -> Json<User> {
-    Json(
-        UserRepository::update(id, &body).await
-            .expect(format!("Not able to update user (id:{}) with payload: {:?}", id, &body).as_str()
-            )
-    )
+async fn update_user(Path(id): Path<u64>, Json(body): Json<User>) -> Response {
+    UserRepository::update(id, &body).await.into_response()
 }
 
-async fn delete_user(Path(id): Path<u64>) {
-    UserRepository::delete(id).await;
+async fn delete_user(Path(id): Path<u64>) -> Response {
+    match UserRepository::delete(id).await {
+        Ok(_) => (StatusCode::NO_CONTENT).into_response(),
+        Err(err) => err.into_response()
+    }
 }
-
-
