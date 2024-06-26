71c1ba705567de6050e4df82dc2ea53c659d142e
Jakub Baran <kubabaran2000@wp.pl>
2024-06-26 23:58:11 +0200

    Better app config

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,17 @@
+use crate::server::Server;
+
 mod entities;
 mod repositories;
-mod migrations;
 mod error;
 mod server;
 mod controllers;
 mod database;
-
-use std::env;
-use dotenv::dotenv;
-use migrations::Migrations;
-use crate::database::{CONNECTION, Database};
-use crate::server::Server;
+mod config;
 
 #[tokio::main]
 async fn main() {
-    load_dot_env();
-    Database::new()
-        .create_db_if_not_exists().await
-        .establish_connection().await;
-    Migrations::run_migrations(CONNECTION.get().unwrap()).await;
+    // Database::new()
+    //     .create_db_if_not_exists().await
+    //     .establish_connection().await;
     Server::run().await;
-}
-
-fn load_dot_env() {
-    dotenv().ok();
-}
-
-pub fn development_mode() -> bool {
-    env::var("DEVELOPMENT_MODE")
-        .unwrap_or("false".to_string())
-        .eq_ignore_ascii_case("true")
 }
