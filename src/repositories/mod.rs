2406d9a1150bf8bd9da87f3a6bab818b04c7d0dd
Jakub Baran <kubabaran2000@wp.pl>
2024-07-04 01:31:24 +0200

    working connection with mongodb

--- src/repositories/mod.rs
+++ src/repositories/mod.rs
@@ -5,18 +5,14 @@ pub mod user_repository;
 
 #[async_trait]
 pub trait Repository<T> {
-    const SELECT_ALL_QUERY: &'static str;
+
     async fn get_all() -> Result<Vec<T>, HttpError>;
 
-    const SELECT_BY_ID_QUERY: &'static str;
     async fn get_by_id(id: u64) -> Result<T, HttpError>;
 
-    const INSERT_QUERY: &'static str;
     async fn create(entity: &T) -> Result<T, HttpError>;
 
-    const UPDATE_QUERY: &'static str;
     async fn update(id: u64, entity: &T) -> Result<T, HttpError>;
 
-    const DELETE_QUERY: &'static str;
     async fn delete(id: u64) -> Result<(), HttpError>;
 }
